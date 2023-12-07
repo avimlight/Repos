18b6c1ae13bbeca0a91591eb58c189070f17a2d4
avimlight <143285108+avimlight@users.noreply.github.com>
2023-12-07 07:31:34 +0530

    Variablebasics

---
+++ variablebasic.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << sizeof(int) << endl;
+    ;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << INT_MIN << endl;
+    cout << INT_MAX << endl;
+    cout << INT_MAX - INT_MIN << endl;
+    cout << stoi("1") << endl; // implementing string to integr
+    cout << stof("1.23") << endl;
+    cout << 4 / 5 << endl;
+    char d = '$';
+    cout << int(d) << endl;
+    cout << float(4 / 5) << endl;
+    cout << 4.0 / 5 << endl;
+    char c = 'a';
+    cout << int(c) << endl;
+    // conversion to the string is
+    cout << to_string(1) << endl;
+    return 0;
+}
