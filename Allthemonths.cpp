1d89fcbf3a097ad0cbad8a35fbbc4494da16d0be
avimlight <143285108+avimlight@users.noreply.github.com>
2023-12-12 10:26:22 +0530

    All the months

---
+++ Allthemonths.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+	vector<string> MonthsOfTheYear () {
+		// Your code goes here
+		return {"January","February","March","April","May","June","July","August","September","October","November","December"};
+	}
+};
