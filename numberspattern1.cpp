a7ae4c93f4a54da0f973fa6fe5af307b261e0f56
avimlight <143285108+avimlight@users.noreply.github.com>
2023-12-12 10:08:38 +0530

    Numberpattern impementation

---
+++ numberspattern1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	void TimesTable (int n) {
+		// Your code goes here
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				cout<<i*j<<" ";
+			}
+cout<<endl;
+		}
+	
+	}
+};
