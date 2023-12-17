4a1c7ec5e72c35a60d23763fe81f75bbbf9f0505
avimlight <143285108+avimlight@users.noreply.github.com>
2023-12-17 23:47:36 +0530

    Implementing pointer using array

---
+++ pointers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int *ptr;
+    ptr = &arr[0];
+    cout << *ptr << endl;
+    cout << *(ptr + 1) << endl;
+    // cool approach
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        cout << *(ptr + i) << endl;
+    }
+    return 0;
+}
