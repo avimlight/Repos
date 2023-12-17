b37d7b86ac26ffde4c05b5e19a9815c185d9a06b
avimlight <143285108+avimlight@users.noreply.github.com>
2023-12-17 22:31:22 +0530

    Class implemntation using pointers

---
+++ oof2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Student
+{
+public:
+    int rollno;
+    string name;
+
+    void f()
+    {
+        cout << "this is the way" << endl;
+        cout << "the name is" << name << endl;
+    }
+    void gh();
+    void rh();
+};
+void Student ::gh()
+{
+    cout << "this is the implementation for scope resolution operator" << endl;
+}
+void Student::rh()
+{
+    cout << "what to say man u came to see what i wrote" << endl;
+}
+int main()
+{
+    Student s1, s2;
+    s1.rollno = 55;
+    s1.name = "sumith";
+    s1.f();
+    s2.rollno = 78;
+    s2.f();
+    s2.gh();
+    s2.rh();
+    Student *s3;
+    s3->rollno = 23;
+    cout << "the value of the roolno is" << s3->rollno << endl;
+    return 0;
+}
